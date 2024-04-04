4f3661ccd4921f736cf82126c75efae078d7b030
Luis Felipe <luisrochacruzalves@gmail.com>
2024-04-04 20:25:31 -0300

    feat: create home structure

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,25 +1,31 @@
 import { styled } from "@/styles";
+import { HomeContainer, Product } from "@/styles/pages/home";
+import Image from "next/image";
 
-const Button = styled('button', {
-  backgroundColor: '$green300',
-  borderRadius: 4,
-  border: 0,
-  padding: '4px 8px',
-
-  span: {
-    fontWeight: 'bold'
-  },
-
-  '&hover': {
-    filter: 'brightness(0.8)'
-  }
-})
+import Camiseta1 from '../assets/camisetas/1.png'
+import Camiseta2 from '../assets/camisetas/2.png'
+import Camiseta3 from '../assets/camisetas/3.png'
 
 export default function Home() {
   return (
-    <Button>
-      <span>Teste</span>
-      Enviar
-    </Button>
+    <HomeContainer>
+      <Product>
+        <Image src={Camiseta1} width={520} height={480} alt=""/>
+
+        <footer>
+          <strong>Camiseta X</strong>
+          <span>R$ 79,90</span>
+        </footer>
+      </Product>
+
+      <Product>
+        <Image src={Camiseta1} width={520} height={480} alt=""/>
+
+        <footer>
+          <strong>Camiseta X</strong>
+          <span>R$ 79,90</span>
+        </footer>
+      </Product>
+    </HomeContainer>
   );
 }
